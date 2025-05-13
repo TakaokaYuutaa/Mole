4e555f068973b59145bd32f63dd6b2166e96dea6
Taokata_yu <dorasuke5897@gmail.com>
2025-05-13 16:40:04 +0900

    MoleProgramプログラム_完

--- Assets/Scripts/MoleProgram.cs
+++ Assets/Scripts/MoleProgram.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -9,16 +10,19 @@ public class MoleProgram : MonoBehaviour
     [SerializeField] Mole_InOut _inOut;
     void Start()
     {
-       
+        var _ = MoleAction();
     }
     void OnMouseDown()
     {
         _inOut.judgMolesOut[_inOut.holeNo] = false;
-        Destroy(this);
+        Destroy(this.gameObject);
     }
     async UniTask MoleAction()
     {
-
+        this.transform.DOMoveY(5, 0.8f).SetRelative();
         await UniTask.Delay(800);
+        this.transform.DOMoveY(-5, 0.8f).SetRelative();
+        Destroy(this.gameObject);
+        _inOut.judgMolesOut[_inOut.holeNo] = false;
     }
 }
