eab77445094974f34357c460a92e664fd53b5169
Taokata_yu <dorasuke5897@gmail.com>
2025-05-13 15:06:16 +0900

    UniTask導入

---
+++ Assets/Scripts/Mole_InOut.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole_InOut : MonoBehaviour
+{
+    [SerializeField] GameObject _mole;
+    private void Start()
+    {
+        
+    }
+}
