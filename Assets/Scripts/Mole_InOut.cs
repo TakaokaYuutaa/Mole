998865846531bc7d61184ecc252b8c47f1f3abe1
Taokata_yu <dorasuke5897@gmail.com>
2025-05-16 19:31:06 +0900

    mole_inoutスクリプト完成

--- Assets/Scripts/Mole_InOut.cs
+++ Assets/Scripts/Mole_InOut.cs
@@ -9,7 +9,7 @@ public class Mole_InOut : MonoBehaviour
     public int holeNo;
     public List<bool> judgMolesOut = new()
     {
-        false, false, false, 
+        false, false, false,
         false, false, false,
         false, false, false,
     };
@@ -33,27 +33,32 @@ public class Mole_InOut : MonoBehaviour
     async UniTask RandomDelay()
     {
         int delayTimeX = 50;
-        int delayTimeY = 100;
-        await UniTask.Delay(Random.Range(delayTimeX,delayTimeY) * 100);
+        int delayTimeY = 50;
+        await UniTask.Delay(Random.Range(delayTimeX, delayTimeY) * 100);
         AdventMole(molesPostion);
     }
     void AdventMole(float molePos)
     {
+        float moleYposition=-5;
         int holeCount = 3;
         if (gamePlay)
         {
             Vector2 position;
             position.x = Random.Range(0, holeCount) * molesPostion;
             position.y = Random.Range(0, holeCount) * molesPostion;
-            _mole.gameObject.transform.position = new Vector3(position.x, -5, position.y);
-            for (int i = 0; i < judgMolesOut.Count; i++)
+            _mole.gameObject.transform.position = new Vector3(position.x, moleYposition, position.y);
+            for (int i = 0; i < holeCount; i++)
             {
-                if (position.x == molesPostion * (molesPostion%holeCount) && position.y == molesPostion * (molesPostion%holeCount) && judgMolesOut[i] == false)
+                for (int j = 0; j < holeCount; j++)
                 {
-                    judgMolesOut[i] = true;
-                    AddMole();
-                    holeNo = i;
-                }
+                    if (position.x == molesPostion * i && position.y == molesPostion * j && judgMolesOut[i+j] == false)
+                    {
+                        judgMolesOut[i+j] = true;
+                        AddMole();
+                        holeNo = i+j;
+                        break;
+                    }
+                }   
             }
         }
     }
